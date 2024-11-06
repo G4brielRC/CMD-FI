862c6b50b191865c9d082eded2b9e1496fb218ff
Gabriel Crepaldi <gabriel.crepaldi9@etec.sp.gov.br>
2024-11-06 20:12:41 -0300

    Add files via upload

---
+++ cmd.bat
@@ -0,0 +1,39 @@
+
+cd..
+cd..
+vol
+ver
+cls
+dir
+md Huguinho Zezinho Luizinho
+dir
+cd Luizinho 
+md Brasil Argentina Uruguai
+
+cd..
+cd Zezinho 
+md Brasil Argentina Uruguai
+
+cd..
+cd Huguinho 
+md Brasil Argentina Uruguai
+rd Argentina
+dir
+
+cd..
+cd Zezinho
+rd Argentina
+dir
+cd..
+cd Luizinho
+rd Argentina
+dir
+
+echo Este é um arquivo de log > C:\log.txt
+echo Arquivo log.txt criado com sucesso na unidade C:.
+copy C:\log.txt C:\Huguinho\log.txt
+copy C:\log.txt C:\Zezinho\log.txt
+copy C:\log.txt C:\Luizinho\log.txt
+pause
+
+
